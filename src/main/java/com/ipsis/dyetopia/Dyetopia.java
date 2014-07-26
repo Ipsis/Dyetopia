2e072d98caec3bdf048ccce9787e493d4c782eb9
Ipsis <ipsis418@gmail.com>
2014-07-26 11:51:57 +0100

    Initial project structure

---
+++ src/main/java/com/ipsis/dyetopia/Dyetopia.java
@@ -0,0 +1,10 @@
+package com.ipsis.dyetopia;
+
+import com.ipsis.dyetopia.reference.Reference;
+import cpw.mods.fml.common.Mod;
+
+@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Dyetopia.VERSION)
+public class Dyetopia {
+
+    public static final String VERSION = "1.7.10-0.1a";
+}
---
+++ src/main/java/com/ipsis/dyetopia/reference/Reference.java
@@ -0,0 +1,7 @@
+package com.ipsis.dyetopia.reference;
+
+public class Reference {
+
+    public static final String MOD_ID = "dyetopia";
+    public static final String MOD_NAME = "Dyetopia";
+}
