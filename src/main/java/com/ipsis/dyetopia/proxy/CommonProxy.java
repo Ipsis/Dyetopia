eaac3f30da976c9a831c2b56083eb964972525f6
Ipsis <ipsis418@gmail.com>
2014-08-02 09:12:03 +0100

    Handle mod liquid pickup with empty bucket.

--- src/main/java/com/ipsis/dyetopia/proxy/CommonProxy.java
+++ src/main/java/com/ipsis/dyetopia/proxy/CommonProxy.java
@@ -1,5 +1,8 @@
 package com.ipsis.dyetopia.proxy;
 
+import com.ipsis.dyetopia.block.DYTBlocks;
+import com.ipsis.dyetopia.handler.BucketHandler;
+import com.ipsis.dyetopia.item.DYTItems;
 import net.minecraftforge.client.event.TextureStitchEvent;
 import net.minecraftforge.common.MinecraftForge;
 
@@ -12,6 +15,13 @@ public abstract class CommonProxy implements IProxy {
     public void registerEventHandlers() {
 
         MinecraftForge.EVENT_BUS.register(this);
+
+        BucketHandler.INSTANCE.buckets.put(DYTBlocks.blockFluidDyeRed, DYTItems.bucketDyeRed);
+        BucketHandler.INSTANCE.buckets.put(DYTBlocks.blockFluidDyeYellow, DYTItems.bucketDyeYellow);
+        BucketHandler.INSTANCE.buckets.put(DYTBlocks.blockFluidDyeBlue, DYTItems.bucketDyeBlue);
+        BucketHandler.INSTANCE.buckets.put(DYTBlocks.blockFluidDyeWhite, DYTItems.bucketDyeWhite);
+        BucketHandler.INSTANCE.buckets.put(DYTBlocks.blockFluidDyePure, DYTItems.bucketDyePure);
+        MinecraftForge.EVENT_BUS.register(BucketHandler.INSTANCE);
     }
 
     public void textureHook(TextureStitchEvent.Post event) {
