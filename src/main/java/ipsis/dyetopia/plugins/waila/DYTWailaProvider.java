d3c176e68c06e8805db557f27633db40aa4d6b68
Ipsis <ipsis418@gmail.com>
2014-12-07 12:30:13 +0000

    Add Waila support for all the machines.

--- src/main/java/ipsis/dyetopia/plugins/waila/DYTWailaProvider.java
+++ src/main/java/ipsis/dyetopia/plugins/waila/DYTWailaProvider.java
@@ -3,6 +3,7 @@ package ipsis.dyetopia.plugins.waila;
 
 import cofh.api.energy.EnergyStorage;
 import cofh.lib.util.helpers.StringHelper;
+import ipsis.dyetopia.manager.TankManager;
 import ipsis.dyetopia.tileentity.*;
 import ipsis.dyetopia.util.TankType;
 import mcp.mobius.waila.api.IWailaConfigHandler;
@@ -11,11 +12,24 @@ import mcp.mobius.waila.api.IWailaDataProvider;
 import mcp.mobius.waila.api.IWailaRegistrar;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.EnumChatFormatting;
+import net.minecraft.util.StatCollector;
 import net.minecraftforge.fluids.FluidTank;
 
 import java.util.List;
 
+/**
+ * Power is already handled by Waila with IEnergyHandler support
+ */
 public class DYTWailaProvider implements IWailaDataProvider{
+
+    /* Waila API formatting values from waila/api/SpecialChars.java */
+    private static String WailaStyle     = "\u00A4";
+    private static String TAB         = WailaStyle + WailaStyle +"a";
+    private static String ALIGNRIGHT  = WailaStyle + WailaStyle +"b";
+    private static String ALIGNCENTER = WailaStyle + WailaStyle +"c";
+
+
     @Override
     public ItemStack getWailaStack(IWailaDataAccessor accessor, IWailaConfigHandler config) {
         return null;
@@ -29,12 +43,28 @@ public class DYTWailaProvider implements IWailaDataProvider{
     @Override
     public List<String> getWailaBody(ItemStack itemStack, List<String> currenttip, IWailaDataAccessor accessor, IWailaConfigHandler config) {
 
+        /**
+         * Waila will autodisplay the RF via an IEnergyHandler hooks
+         * Anything else (tanks for now) only shows on sneak
+         */
+        if (!accessor.getPlayer().isSneaking())
+            return currenttip;
 
         if (accessor.getTileEntity() instanceof TileEntitySqueezer) {
-
             TileEntitySqueezer te = (TileEntitySqueezer)accessor.getTileEntity();
             currenttip = handleTileEntitySqueezer((TileEntitySqueezer)accessor.getTileEntity(), currenttip);
-
+        } else if (accessor.getTileEntity() instanceof TileEntityMixer) {
+            TileEntityMixer te = (TileEntityMixer)accessor.getTileEntity();
+            currenttip = handleTileEntityMixer((TileEntityMixer) accessor.getTileEntity(), currenttip);
+        } else if (accessor.getTileEntity() instanceof TileEntityPainter) {
+            TileEntityPainter te = (TileEntityPainter)accessor.getTileEntity();
+            currenttip = handleTileEntityPainter((TileEntityPainter) accessor.getTileEntity(), currenttip);
+        }  else if (accessor.getTileEntity() instanceof TileEntityStamper) {
+            TileEntityStamper te = (TileEntityStamper)accessor.getTileEntity();
+            currenttip = handleTileEntityStamper((TileEntityStamper) accessor.getTileEntity(), currenttip);
+        }  else if (accessor.getTileEntity() instanceof TileEntityFiller) {
+            TileEntityFiller te = (TileEntityFiller)accessor.getTileEntity();
+            currenttip = handleTileEntityFiller((TileEntityFiller) accessor.getTileEntity(), currenttip);
         } else if (accessor.getTileEntity() instanceof TileEntityMultiBlockBase) {
 
             TileEntityMultiBlockBase base = (TileEntityMultiBlockBase)accessor.getTileEntity();
@@ -42,6 +72,8 @@ public class DYTWailaProvider implements IWailaDataProvider{
                 TileEntity te = accessor.getWorld().getTileEntity(base.getMasterX(), base.getMasterY(), base.getMasterZ());
                 if (te instanceof TileEntitySqueezer)
                     currenttip = handleTileEntitySqueezer((TileEntitySqueezer) te, currenttip);
+                else if (te instanceof TileEntityMixer)
+                    currenttip = handleTileEntityMixer((TileEntityMixer) te, currenttip);
             }
         }
 
@@ -56,6 +88,11 @@ public class DYTWailaProvider implements IWailaDataProvider{
     public static void callbackRegister(IWailaRegistrar registrar){
 
         registrar.registerBodyProvider(new DYTWailaProvider(), TileEntitySqueezer.class);
+        registrar.registerBodyProvider(new DYTWailaProvider(), TileEntityMixer.class);
+        registrar.registerBodyProvider(new DYTWailaProvider(), TileEntityPainter.class);
+        registrar.registerBodyProvider(new DYTWailaProvider(), TileEntityStamper.class);
+        registrar.registerBodyProvider(new DYTWailaProvider(), TileEntityFiller.class);
+
         registrar.registerBodyProvider(new DYTWailaProvider(), TileEntityValve.class);
         registrar.registerBodyProvider(new DYTWailaProvider(), TileEntityCasing.class);
         registrar.registerBodyProvider(new DYTWailaProvider(), TileEntityController.class);
@@ -69,41 +106,57 @@ public class DYTWailaProvider implements IWailaDataProvider{
         if (!te.isStructureValid())
             return currenttip;
 
-        displayEnergy(currenttip, te.getEnergyMgr().getEnergyStorage());
-
-        displayNamedTankInfo(currenttip, "Red", te.getTankMgr().getTank(TankType.RED.getName()));
-        displayNamedTankInfo(currenttip, "Yellow", te.getTankMgr().getTank(TankType.YELLOW.getName()));
-        displayNamedTankInfo(currenttip, "Blue", te.getTankMgr().getTank(TankType.BLUE.getName()));
-        displayNamedTankInfo(currenttip, "White", te.getTankMgr().getTank(TankType.WHITE.getName()));
-
+        displayTankManager(te.getTankMgr(), currenttip, TankType.RED, TankType.YELLOW, TankType.BLUE, TankType.WHITE);
         return currenttip;
     }
 
-    private void displayEnergy(List<String> currenttip, EnergyStorage storage) {
+    private List<String> handleTileEntityMixer(TileEntityMixer te, List<String> currenttip) {
 
-        currenttip.add(String.format("Energy : %d / %d RF",
-                storage.getEnergyStored(),
-                storage.getMaxEnergyStored()));
+        if (!te.isStructureValid())
+            return currenttip;
+
+        displayTankManager(te.getTankMgr(), currenttip, TankType.RED, TankType.YELLOW, TankType.BLUE, TankType.WHITE, TankType.PURE);
+        return currenttip;
     }
 
-    /**
-     * Display name, not the fluid name
-     */
-    private void displayNamedTankInfo(List<String> currenttip, String name, FluidTank tank) {
-        if (tank != null)
-            currenttip.add(String.format("%s : %d / %d mB", name, tank.getFluidAmount(), tank.getCapacity()));
+    private List<String> handleTileEntityPainter(TileEntityPainter te, List<String> currenttip) {
+
+        displayTankManager(te.getTankMgr(), currenttip, TankType.PURE);
+        return currenttip;
     }
 
-    /**
-     * Display the fluid name if the tank is not empty
-     */
-    private void displayTankInfo(List<String> currenttip, FluidTank tank) {
+    private List<String> handleTileEntityFiller(TileEntityFiller te, List<String> currenttip) {
 
-        if (tank != null) {
-            if (tank.getFluid() != null)
-                currenttip.add(String.format("%s : %d / %d mB",
-                        StringHelper.getFluidName(tank.getFluid()),
-                        tank.getFluid().fluidID, tank.getFluidAmount(), tank.getCapacity()));
+        displayTankManager(te.getTankMgr(), currenttip, TankType.PURE);
+        return currenttip;
+    }
+
+    private List<String> handleTileEntityStamper(TileEntityStamper te, List<String> currenttip) {
+
+        displayTankManager(te.getTankMgr(), currenttip, TankType.PURE);
+        return currenttip;
+    }
+
+    private List<String> displayTankManager(TankManager tm, List<String> currenttip, TankType... names) {
+
+        if (tm == null || names.length == 0)
+            return currenttip;
+
+        currenttip.add(EnumChatFormatting.YELLOW + "Tanks");
+        for (TankType name : names) {
+
+            FluidTank tank = tm.getTank(name.getName());
+            if (tank == null)
+                continue;
+
+            currenttip.add(EnumChatFormatting.ITALIC + String.format("%s : %s%d/%d%smB",
+                    StatCollector.translateToLocal("hud.msg.dyetopia:tank." + name.getName()),
+                    TAB + ALIGNRIGHT,
+                    tank.getFluidAmount(),
+                    tank.getCapacity(),
+                    TAB + ALIGNRIGHT));
         }
+
+        return currenttip;
     }
 }
