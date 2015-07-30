f9c43d7fbc7fd14e5bbb16157103d802c31a191f
Ipsis <ipsis418@gmail.com>
2015-07-30 12:24:42 +0100

    Get list of blocks a certain radius away in all directions

--- src/main/java/ipsis/dyetopia/util/WorldHelper.java
+++ src/main/java/ipsis/dyetopia/util/WorldHelper.java
@@ -1,8 +1,12 @@
 package ipsis.dyetopia.util;
 
 
+import cofh.lib.util.position.BlockPosition;
 import net.minecraft.block.Block;
 import net.minecraft.world.World;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class WorldHelper {
 
@@ -15,5 +19,24 @@ public class WorldHelper {
             b = world.getBlock(x, y, z);
 
         return b;
+    }
+
+    public static List<BlockPosition> getSurroundingBlockPositions(int x, int y, int z, int radius) {
+
+        List<BlockPosition> posList = new ArrayList<BlockPosition>();
+
+        if (radius < 1)
+            return posList;
+
+        for (int yOff = -radius; yOff <= radius; yOff++) {
+            for (int xOff = -radius; xOff <= radius; xOff++) {
+                for (int zOff = -radius; zOff <= radius; zOff++) {
+                    posList.add(new BlockPosition(x + xOff, y + yOff, z + zOff));
+                }
+            }
+        }
+
+        return posList;
+
     }
 }
